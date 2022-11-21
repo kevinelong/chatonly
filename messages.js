ae758e316fd8edc662fa8fef419317ccb78e6d98
kevinelong <kevinelong@gmail.com>
2022-11-20 16:08:05 -0800

    refactor

--- messages.js
+++ messages.js
@@ -1,17 +1,45 @@
-function message(text, isCurrentUser=true, isSamePerson=false, who="", when=undefined) {
-    when = when ? when : new Date();
-    time = when.toLocaleTimeString();
-    const currentUser = isCurrentUser ? "is-current-user" : '';
-    const samePerson = isSamePerson ? "same-person" : '';
-    return `
-    <div class="message-item ${samePerson} ${currentUser} div">
-            <div class="action-item person connect_person div"
-                onclick="actionClick(this, '${who}','connect_person','3')">
-                <div class="icon-frame div"><img class="icon" src="profile-picture.svg"></div>
-                <div class="text wc-1">${who}</div>
-                <div class="text wc-1">${time}</div>
+
+class MessagesView{
+
+    currentUser = "Me";
+    isSamePerson = false;
+
+    constructor(container, textInput){
+        this.container = container;
+        this.textInput = textInput;
+        this.textInput.addEventListener("keyup", this.onTextInput);
+    }
+
+    makeMessage(text, who="", when=undefined) {
+        this.isSamePerson = this.currentUser == who;
+        this.currentUser = who;
+        when = when ? when : new Date();
+        const time = when.toLocaleTimeString();
+        const currentUser = this.isCurrentUser ? "is-current-user" : '';
+        const samePerson = this.isSamePerson ? "same-person" : '';
+        return `
+            <div class="message-item ${samePerson} ${currentUser} div">
+                <div class="action-item person connect_person div"
+                    onclick="actionClick(this, '${who}','connect_person','3')"
+                ><div class="icon-frame div"><img class="icon" src="profile-picture.svg"></div></div>
+                <div class="message-text div">
+                    <div class="text extra">${who} - ${time}</div>
+                    ${text}
+                </div>
             </div>
-            <div class="message-text div">${text}</div>
-        </div>
-    `;
+        `;
+    }
+
+    addMessage(text, who="") {
+        this.container.innerHTML += this.makeMessage(text, who, when);
+    }
+
+    onTextInput(e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            mv.addMessage(e.target.value, "Me");
+            e.target.value = "";
+            return false;
+        }
+    }
 }
